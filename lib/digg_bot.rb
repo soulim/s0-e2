fd39efebb4f6f1e0ec509440db33e9a0ff86f5e8
Alex Soulim <soulim@gmail.com>
2010-08-10 13:22:21 +0400

    Added bin/digg_bot command and updated specs

--- lib/digg_bot.rb
+++ lib/digg_bot.rb
@@ -5,9 +5,36 @@ require 'rubygems'
 require 'cinch'
 require 'digger'
 
-class DiggerBot
+class DiggBot
   def initialize
     @digger = Digger::Api.new
+    @bot    = Cinch.setup do 
+      server    'irc.freenode.org'
+      nick      'diggbot'
+      username  'diggbot'
+    end
+
+    @bot.on(376) do |m|
+      @bot.join '#rmu-session-0', 'zerowing'
+    end
+
+    @bot.plugin("digg top stories") do |m|
+      m.answer  self.top_stories || "No results found"
+    end
+
+    @bot.plugin("digg containers") do |m|
+      m.answer self.containers || "No results found"
+    end
+
+    @bot.plugin("digg hot stories in :container") do |m|
+      m.answer self.hot_stories_from(m.args[:container]) || "No results found"
+    end
+
+    @bot.plugin("digg help") do |m|
+      m.answer "Usage: !digg top stories #=> get top stories, !digg containers #=> list of all containers, !digg hot stories in [container_short_name] #=> list of hot stories from given container"
+    end
+
+    @bot.run
   end
   
   def top_stories(container = nil)
@@ -22,34 +49,3 @@ class DiggerBot
     @digger.story.getHot.options(:count => 3, :container => container).fetch.collect { |story| "'#{story.title}' (#{story.href})" }.join(', ')
   end
 end
-
-
-digger = DiggerBot.new
-
-bot = Cinch.setup do 
-  server    'irc.freenode.org'
-  nick      'diggbot'
-  username  'diggbot'
-end
-
-bot.on(376) do |m|
-  bot.join '#rmu-session-0', 'zerowing'
-end
-
-bot.plugin("digg top stories") do |m|
-  m.answer  digger.top_stories || "No results found"
-end
-
-bot.plugin("digg containers") do |m|
-  m.answer digger.containers || "No results found"
-end
-
-bot.plugin("digg hot stories in :container") do |m|
-  m.answer digger.hot_stories_from(m.args[:container]) || "No results found"
-end
-
-bot.plugin("digg help") do |m|
-  m.answer "Usage: !digg top stories #=> get top stories, !digg containers #=> list of all containers, !digg hot stories in [container_short_name] #=> list of hot stories from given container"
-end
-
-bot.run
--- spec/digg_bot_spec.rb
+++ spec/digg_bot_spec.rb
@@ -2,9 +2,25 @@ require 'spec_helper'
 
 describe DiggBot do
   subject { DiggBot.new }
+  before(:each) do
+    bot = mock('IRC bot', :on => nil, :plugin => nil, :run => nil)
+    Cinch.stub!(:setup).and_return(bot)
+    @digg_api = mock('Digg API').as_null_object
+    Digger::Api.stub!(:new).and_return(@digg_api)
+  end
   
-  it 'gets top stories'
-  it 'gets list of topis'
-  it 'gets list of stories for given topic'
-  it 'trys to find some story for given params'
+  it 'gets top stories' do
+    @digg_api.stub!(:fetch).and_return([Digger::Story.new])
+    subject.top_stories.should_not be_empty
+  end
+  
+  it 'gets list of containers' do
+    @digg_api.stub!(:fetch).and_return([Digger::Container.new])
+    subject.containers.should_not be_empty
+  end
+  
+  it 'gets hot stories for given container' do
+    @digg_api.stub!(:fetch).and_return([Digger::Story.new])
+    subject.hot_stories_from.should_not be_empty
+  end
 end
